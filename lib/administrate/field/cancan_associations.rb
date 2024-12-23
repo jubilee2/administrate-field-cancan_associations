9591a9fda9f585aaca1a84d7be24ba176fc833d2
Jubilee Tan <jubilee2@gmail.com>
2024-12-23 21:09:58 +0000

    first

---
+++ lib/administrate/field/cancan_associations.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require_relative "cancan_associations/version"
+
+module Administrate
+  module Field
+    module CancanAssociations
+      class Error < StandardError; end
+      # Your code goes here...
+    end
+  end
+end
---
+++ spec/administrate/field/cancan_associations_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Administrate::Field::CancanAssociations do
+  it "has a version number" do
+    expect(Administrate::Field::CancanAssociations::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
