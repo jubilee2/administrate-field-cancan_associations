553aa9c3501f0ee5c3649143a4677d9f3ae27d7b
Jubilee Tan <jubilee2@gmail.com>
2024-12-23 21:34:43 +0000

    first  lib/administrate/field/cancan_associations.rb

--- lib/administrate/field/cancan_associations.rb
+++ lib/administrate/field/cancan_associations.rb
@@ -1,12 +1,38 @@
 # frozen_string_literal: true
 
 require_relative "cancan_associations/version"
+require "administrate/field/belongs_to"
+require "rails"
 
 module Administrate
   module Field
-    module CancanAssociations
-      class Error < StandardError; end
-      # Your code goes here...
+    class CancanBelongsTo < Administrate::Field::BelongsTo
+      include CancanAssociations
+      class Engine < ::Rails::Engine; end
+
+      def associated_resource_options(user, ability)
+        candidate_resources(user, ability).map do |resource|
+          [
+            display_candidate_resource(resource),
+            resource.send(association_primary_key)
+          ]
+        end
+      end
+
+      private
+
+      def candidate_resources(user, ability)
+        scope =
+          if options[:scope]
+            options[:scope].call(self, user, ability)
+          else
+            associated_class.all
+          end
+
+        order = options.delete(:order)
+        order ? scope.reorder(order) : scope
+      end
+
     end
   end
 end
